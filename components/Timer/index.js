36ea143df899e23e9388a9216f16741a4b54aa4b
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2018-10-15 23:54:58 +0900

    fix index.js
    Fix words about import, actions

--- components/Timer/index.js
+++ components/Timer/index.js
@@ -1,23 +1,23 @@
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import {actionCreators as timerActions } from '../../reducer';
-import Timer from './presenter';
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators as tomatoActions } from "../../reducer";
+import Timer from "./presenter";
 
-function mapStateProps(state) {
+function mapStateToProps(state) {
   const { isPlaying, elapsedTime, timerDuration } = state;
   return {
     isPlaying,
     elapsedTime,
     timerDuration
-  }
+  };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    startTimer: bindActionCreators(timerActions.startTimer, dispatch),
-    restartTimer: bindActionCreators(timerActions.restartTimer, dispatch),
-    addSecond: bindActionCreators(timerActions.addSecond, dispatch)
-  }
+    startTimer: bindActionCreators(tomatoActions.startTimer, dispatch),
+    restartTimer: bindActionCreators(tomatoActions.restartTimer, dispatch),
+    addSecond: bindActionCreators(tomatoActions.addSecond, dispatch)
+  };
 }
 
-export default connect(mapStateProps, mapDispatchToProps)(Timer);
+export default connect(mapStateToProps, mapDispatchToProps)(Timer);
